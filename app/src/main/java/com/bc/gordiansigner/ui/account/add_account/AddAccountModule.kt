645c94c27fd48bf634cfa96fd8c324b4e318c6e2
hvthhien <hvthhien@gmail.com>
2020-10-16 16:57:27 +0700

    Add signer screen new design.

--- app/src/main/java/com/bc/gordiansigner/ui/account/add_account/AddAccountModule.kt
+++ app/src/main/java/com/bc/gordiansigner/ui/account/add_account/AddAccountModule.kt
@@ -3,6 +3,7 @@ package com.bc.gordiansigner.ui.account.add_account
 import com.bc.gordiansigner.di.ActivityScope
 import com.bc.gordiansigner.helper.livedata.RxLiveDataTransformer
 import com.bc.gordiansigner.service.WalletService
+import com.bc.gordiansigner.ui.DialogController
 import com.bc.gordiansigner.ui.Navigator
 import dagger.Module
 import dagger.Provides
@@ -21,4 +22,8 @@ class AddAccountModule {
     @ActivityScope
     @Provides
     fun provideNavigator(activity: AddAccountActivity) = Navigator(activity)
+
+    @ActivityScope
+    @Provides
+    fun dialogController(activity: AddAccountActivity) = DialogController(activity)
 }
